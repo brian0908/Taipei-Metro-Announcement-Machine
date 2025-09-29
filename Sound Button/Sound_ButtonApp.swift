1c06287bface65877db250e264d63f9cf6beead6
Brian Lee <leebrian0908@gmail.com>
2025-09-29 16:11:45 +0800

    Initial Commit

---
+++ Sound Button/Sound_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sound_ButtonApp.swift
+//  Sound Button
+//
+//  Created by Brian Lee on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Sound_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
